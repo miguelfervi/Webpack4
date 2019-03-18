03a8636b97a8459e4eeca9945609566b475faaf6
Miguel <moreneitorsoftware@gmail.com>
2019-03-18 22:37:41 +0100

    Add css and miss point call api and undo

--- src/index.js
+++ src/index.js
@@ -1,30 +1,59 @@
-import style from "./main.css";
+import "./main.css";
 
-var list = document.getElementById("list");
-var add = document.getElementById("add");
+const list = document.getElementById("list");
+const add = document.getElementById("add");
+const remove = document.getElementById("remove");
+const undo = document.getElementById("undo");
 
-add.addEventListener("click", function() {
+let undoHistory = [];
+
+add.addEventListener("click", () => {
+  let item;
   do {
-    var item = prompt("Inserta algo");
-  } while (item == "");
+    item = prompt("Inserta algo");
+  } while (item === "");
   if (item) {
-    var element = document.createElement("li");
+    let element = document.createElement("div");
+    element.classList.add("item");
     element.appendChild(document.createTextNode(item));
-    element.addEventListener("click", function() {
-      this.classList.toggle("selected");
-    });
-    element.addEventListener("dblclick", function() {
-      this.parentNode.removeChild(this);
-    });
+    addEvents(element);
     list.appendChild(element);
+    undoHistory.push(list);
+    console.log(undoHistory);
   }
 });
 
-remove.addEventListener("click", function() {
-  var list = document.querySelectorAll("li");
+remove.addEventListener("click", () => {
+  let list = document.querySelectorAll("div");
   list.forEach(function(element) {
     element.classList.contains("selected")
       ? element.parentNode.removeChild(element)
       : "";
   });
+});
+
+const addEvents = element => {
+  element.addEventListener("click", () => {
+    element.classList.toggle("selected");
+  });
+  element.addEventListener("dblclick", () => {
+    element.parentNode.removeChild(element);
+  });
+};
+
+undo.addEventListener("click", () => {
+  let list = document.getElementById("list");
+  console.log(undoHistory[0].childNodes);
+  //undoHistory.removeChild(undoHistory.childNodes[undoHistory]);
+  /* var parent = list.parentElement;
+  console.log(list.parentElement);
+  console.log(undoHistory, undoHistory.length);
+  if (undoHistory.length > 1) {
+    list.removeChild(list.lastElementChild);
+    undoHistory = undoHistory.slice(-1, 1);
+
+    list.innerHTML = "";
+
+    parent = undoHistory;
+  }*/
 });
