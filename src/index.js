2438f600feeef20307f7afb0368e32112e5258a6
Miguel Fernández <miguel.fernandez@pccomponentes.com>
2019-03-19 11:23:45 +0100

    Fixed undo only when I add item

--- src/index.js
+++ src/index.js
@@ -1,59 +1,56 @@
-import "./main.css";
+import './main.css'
 
-const list = document.getElementById("list");
-const add = document.getElementById("add");
-const remove = document.getElementById("remove");
-const undo = document.getElementById("undo");
+const list = document.getElementById('list')
+const add = document.getElementById('add')
+const remove = document.getElementById('remove')
+const undo = document.getElementById('undo')
 
-let undoHistory = [];
+let undoHistory = []
 
-add.addEventListener("click", () => {
-  let item;
+add.addEventListener('click', () => {
+  let item
   do {
-    item = prompt("Inserta algo");
-  } while (item === "");
+    item = prompt('Inserta algo')
+  } while (item === '')
   if (item) {
-    let element = document.createElement("div");
-    element.classList.add("item");
-    element.appendChild(document.createTextNode(item));
-    addEvents(element);
-    list.appendChild(element);
-    undoHistory.push(list);
-    console.log(undoHistory);
+    let element = document.createElement('div')
+    element.classList.add('item')
+    element.appendChild(document.createTextNode(item))
+    addEvents(element)
+    list.appendChild(element)
+    undoHistory.push(list)
   }
-});
+})
 
-remove.addEventListener("click", () => {
-  let list = document.querySelectorAll("div");
-  list.forEach(function(element) {
-    element.classList.contains("selected")
-      ? element.parentNode.removeChild(element)
-      : "";
-  });
-});
+remove.addEventListener('click', () => {
+  let list = document.querySelectorAll('div')
+  list.forEach(function (element) {
+    if (element.classList.contains('selected')) {
+      element.parentNode.removeChild(element)
+    }
+  })
+})
 
 const addEvents = element => {
-  element.addEventListener("click", () => {
-    element.classList.toggle("selected");
-  });
-  element.addEventListener("dblclick", () => {
-    element.parentNode.removeChild(element);
-  });
-};
+  element.addEventListener('click', () => {
+    element.classList.toggle('selected')
+  })
+  element.addEventListener('dblclick', () => {
+    element.parentNode.removeChild(element)
+    undoHistory.push(list)
+  })
+}
 
-undo.addEventListener("click", () => {
-  let list = document.getElementById("list");
-  console.log(undoHistory[0].childNodes);
-  //undoHistory.removeChild(undoHistory.childNodes[undoHistory]);
-  /* var parent = list.parentElement;
-  console.log(list.parentElement);
-  console.log(undoHistory, undoHistory.length);
-  if (undoHistory.length > 1) {
-    list.removeChild(list.lastElementChild);
-    undoHistory = undoHistory.slice(-1, 1);
+undo.addEventListener('click', () => {
+  let list = document.querySelector('#list')
 
-    list.innerHTML = "";
+  const arrayNodes = Array.from(undoHistory[0].childNodes)
 
-    parent = undoHistory;
-  }*/
-});
+  while (list.firstChild) {
+    list.removeChild(list.firstChild)
+  }
+  arrayNodes.pop()
+  arrayNodes.forEach(function (element) {
+    list.appendChild(element)
+  })
+})
