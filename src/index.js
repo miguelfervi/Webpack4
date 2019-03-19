508729df03dd69766ee1c49654e8eb1ba2d0fcbf
Miguel <moreneitorsoftware@gmail.com>
2019-03-19 19:16:26 +0100

    Added code except issue with the button event

--- src/index.js
+++ src/index.js
@@ -1,79 +1,62 @@
-import './main.css'
+import "./main.css";
 
-const add = document.getElementById('add')
-const remove = document.getElementById('remove')
-const undo = document.getElementById('undo')
-const totalList = document.getElementById('list')
-let children = null
+const add = document.getElementById("add");
+const remove = document.getElementById("remove");
+const undo = document.getElementById("undo");
+const totalList = document.getElementById("list");
+let children = null;
 
-let undoHistory = []
-let item = null
+const undoHistory = [];
+let item = null;
 
-add.addEventListener('click', () => {
+add.addEventListener("click", () => {
   do {
-    item = prompt('Introduce un item')
-  } while (item === '')
+    item = prompt("Introduce un item");
+  } while (item.trim === "");
   if (item) {
-    let element = document.createElement('div')
-    element.classList.add('item')
-    element.appendChild(document.createTextNode(item))
-    addEvents(element)
-    totalList.appendChild(element)
-    children = totalList.innerHTML
-    undoHistory.push(children)
-    console.log(undoHistory)
+    const element = document.createElement("div");
+    element.classList.add("item");
+    element.appendChild(document.createTextNode(item));
+    addEvents(element);
+    totalList.appendChild(element);
+    children = totalList.innerHTML;
+    undoHistory.push(children);
   }
-})
-remove.addEventListener('click', () => {
-  const items = document.querySelectorAll('.item')
-
-  items.forEach((element) => {
-    if (element.classList.contains('selected')) {
-      element.parentNode.removeChild(element)
-      children = totalList.innerHTML
-      undoHistory.push(children)
-      console.log(undoHistory)
-    }
-  })
-})
+});
 
 const addEvents = element => {
-  element.addEventListener('click', () => {
-    element.classList.toggle('selected')
-  })
-  element.addEventListener('dblclick', () => {
-    element.parentNode.removeChild(element)
-    children = totalList.innerHTML
-    undoHistory.push(children)
-    console.log(undoHistory)
-  })
-}
+  console.log(element);
+  element.addEventListener("click", () => {
+    element.classList.toggle("selected");
+    children = totalList.innerHTML;
+    undoHistory.push(children);
+    console.log(undoHistory);
+  });
+  element.addEventListener("dblclick", () => {
+    element.parentNode.removeChild(element);
+    children = totalList.innerHTML;
+    undoHistory.push(children);
+    console.log(undoHistory);
+  });
+};
 
-undo.addEventListener('click', () => {
-  totalList.innerHTML = ''
-  if (undoHistory) {
-    console.log(undoHistory[undoHistory.length - 1])
-    totalList.innerHTML = undoHistory[undoHistory.length - 1]
-    undoHistory.pop()
+remove.addEventListener("click", () => {
+  children.forEach(element => {
+    if (element.classList.contains("selected")) {
+      element.parentNode.removeChild(element);
+      children = totalList.innerHTML;
+      undoHistory.push(children);
+      console.log(undoHistory);
+    }
+  });
+});
+
+undo.addEventListener("click", () => {
+  console.log("Entro");
+  totalList.innerHTML = "";
+  undoHistory.pop();
+  if (undoHistory.length > 0) {
+    totalList.innerHTML = undoHistory[undoHistory.length - 1];
   }
-})
-
-/**
- * var list = document.getElementById('list');
-undefined
-var children = list.innerHTML;
-undefined
-list.innerHTML = ''
-""
-list.innerHTML = children
-"<div class="item">test</div>"
-var array = []
-undefined
-array.push(children)
-1
-array
-["<div class="item">test</div>"]
-array.pop()
-"<div class="item">test</div>"
-array.pop()
- */
+  console.log(undoHistory);
+});
